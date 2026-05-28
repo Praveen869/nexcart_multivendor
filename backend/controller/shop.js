793e43785bf92b7606de1706ab32cadf98af4d4a
Praveen869 <praveenkrdwivedi.3249@gmail.com>
2026-05-28 17:46:49 +0530

    email

--- backend/controller/shop.js
+++ backend/controller/shop.js
@@ -45,7 +45,7 @@ router.post("/create-shop", upload.single("file"), async (req, res, next) => {
 
     const activationToken = createActivationToken(seller);
 
-    const activationUrl = `http://localhost:3000/seller/activation/${activationToken}`;
+    const activationUrl = `${process.env.FRONTEND_URL}/seller/activation/${activationToken}`;
 
     try {
       await sendMail({
--- backend/controller/user.js
+++ backend/controller/user.js
@@ -43,7 +43,7 @@ router.post("/create-user", upload.single("file"), async (req, res, next) => {
 
     const activationToken = createActivationToken(user);
 
-    const activationUrl = `http://localhost:3000/activation/${activationToken}`;
+    const activationUrl = `${process.env.FRONTEND_URL}/activation/${activationToken}`;
 
     // send email to user
     try {
